fc9901c73ac26cb37499243f0cee2c38072ef6f5
strongconjunctions <strongconjunctions@gmail.com>
2016-04-23 23:48:46 +0100

    font changes

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ $(window).scroll(function() {
     $('#banner-area').css({
         // we're manipulating the 'translate' value of the 'transform' property
         // so what we need is to divide the pace of the scroll by 2
-        'transform': 'translate(0px, ' + Scroll / 100 +'%)'
+        'transform': 'translate(0px, ' + Scroll / 90 +'%)'
     });
 
     $('.banner').css({
-        'transform': 'translate(0px, -' + Scroll / 5 +'%)'
+        'transform': 'translate(0px, -' + Scroll / 4.8 +'%)'
     });
 
 });
