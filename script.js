e68f32c736f3c425a1bd41bd427b2fb627948e2b
strongconjunctions <strongconjunctions@gmail.com>
2016-04-24 01:38:11 +0100

    changed the nav a bit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(window).scroll(function() {
     $('#banner-area').css({
         // we're manipulating the 'translate' value of the 'transform' property
         // so what we need is to divide the pace of the scroll by 2
-        'transform': 'translate(0px, ' + Scroll / 90 +'%)'
+        'transform': 'translate(0px, ' + Scroll / 80 +'%)'
     });
 
     $('.banner').css({
