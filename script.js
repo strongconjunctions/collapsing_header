4da526c8ab9b98013e9e2532b56c1d4627195afd
strongconjunctions <strongconjunctions@gmail.com>
2016-04-25 23:52:45 +0100

    redid the sub-sub-nav

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-$(window).scroll(function() {
-
-    // scrollTop is a jQuery object that will tell you how much from the window you've scrolled
-    var Scroll = $(this).scrollTop();
 
 
-    $('#banner-area').css({
-        // we're manipulating the 'translate' value of the 'transform' property
-        // so what we need is to divide the pace of the scroll by 2
-        'transform': 'translate(0px, ' + Scroll / 80 +'%)'
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+
+        // scrollTop is a jQuery object that will tell you how much from the window you've scrolled
+        var Scroll = $(this).scrollTop();
+
+
+        $('#banner-area').css({
+            // we're manipulating the 'translate' value of the 'transform' property
+            // so what we need is to divide the pace of the scroll by 2
+            'transform': 'translate(0px, ' + Scroll / 80 +'%)'
+        });
+
+        $('.banner').css({
+            'transform': 'translate(0px, -' + Scroll / 4.8 +'%)'
+        });
+
     });
 
-    $('.banner').css({
-        'transform': 'translate(0px, -' + Scroll / 4.8 +'%)'
+    $('.nav-toggle').on('click', function() {
+        $('.main-nav').toggleClass('open-nav');
+        $('.nav-toggle').toggleClass('fa-bars');
     });
 
+    $('.sub-nav li').on('click', function() {
+        $('.sub-sub-nav').toggleClass('open-sub-sub');
+    });
 });
