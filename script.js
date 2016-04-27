e21df8a4fc1f5805bb667585e6b6518a2eaec660
strongconjunctions <strongconjunctions@gmail.com>
2016-04-27 23:20:25 +0100

    added new section

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
         // scrollTop is a jQuery object that will tell you how much from the window you've scrolled
         var Scroll = $(this).scrollTop();
 
-        $('.registration').css({
+        /*$('.registration').css({
             'transform': 'translate(0px, -' + Scroll / 2.2 +'%)'
         });
-
+*/
         $('#banner-area').css({
             // we're manipulating the 'translate' value of the 'transform' property
             // so what we need is to divide the pace of the scroll by 2
