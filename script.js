5b2a0d9481b2f8c98b0313732baf8333fb1a8df4
strongconjunctions <strongconjunctions@gmail.com>
2016-04-29 21:53:57 +0100

    changes to mob UI

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ $(document).ready(function() {
         // scrollTop is a jQuery object that will tell you how much from the window you've scrolled
         var Scroll = $(this).scrollTop();
 
-        /*$('.registration').css({
-            'transform': 'translate(0px, -' + Scroll / 2.2 +'%)'
-        });
-*/
         $('#banner-area').css({
             // we're manipulating the 'translate' value of the 'transform' property
             // so what we need is to divide the pace of the scroll by 2
-            'transform': 'translate(0px, ' + Scroll / 120 +'%)'
+            'transform': 'translate(0px, ' + Scroll / 100 +'%)'
         });
+
 
         $('.banner').css({
             'transform': 'translate(0px, -' + Scroll / 5.96 +'%)'
@@ -47,7 +44,7 @@ $(document).ready(function() {
 
    // Opens sub-sub in mobile view
     $('.sub-nav li').on('click', function() {
-        $('.sub-sub-nav').toggleClass('open-sub-sub');
+        $('.sub-sub-nav').toggleClass('open-mob-sub-sub');
     });
 
     // Closes the main-nav panel
@@ -57,14 +54,14 @@ $(document).ready(function() {
     });
  
     // Opens the sub-nav panel in mobile view
-    /*$('.main-nav li').on('click', function() {
+    $('.main-nav li').on('click', function() {
         $('.sub-nav').addClass('open-mob-sub-nav');
     });
-*/
+
 
     // Closes the sub-nav panel
     $('.sub-nav .fa-circle-o').on('click', function() {
-        $('.sub-nav').removeClass('open-mob-sub-nav');
+        $('.sub-nav').removeClass('open-mob-sub-nav open-sub-nav');
     });
 
 });
